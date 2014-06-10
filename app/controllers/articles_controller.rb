e6470baeeddf70dface8d9216ad46746030edff4
daniel <stolichnaya2988@users.noreply.github.com>
2014-06-10 22:35:18 +0700

    Added some validation.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,7 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def show
@@ -12,8 +13,11 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   private
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article
   include Mongoid::Document
   field :title, type: String
   field :text, type: String
+  validates :title, presence: true,
+                    length: { minimum: 5 }
 end
